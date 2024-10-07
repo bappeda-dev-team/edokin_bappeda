36f0de427861251aa1f899d085074229a55751a8
Eksa Lailia <eksalailia@gmail.com>
2024-10-07 13:47:39 +0700

    menambahkan halaman login

---
+++ database/seeders/DataNewSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class DataNewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123456'),
+                // 'role' => 'admin', // Uncomment if you have a role column
+            ],
+        ]);
+    }
+}
--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -15,7 +15,7 @@
       
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
         <img alt="image" src="{{asset('style/dist/assets/img/avatar/avatar-1.png')}}" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Admin!</div></a>
+        <div class="d-sm-none d-lg-inline-block">{{ Auth::user()->name }}</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           {{-- <div class="dropdown-title">Logged in 5 min ago</div> --}}
           <a href="features-profile.html" class="dropdown-item has-icon">
@@ -29,6 +29,11 @@
           </a> --}}
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item has-icon text-danger">
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
+          <a href="#" class="dropdown-item has-icon text-danger" 
+             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="fas fa-sign-out-alt"></i> Logout
           </a>
         </div>
